0d9bfcccb5c70776faba80a5e839a21bb2e25cf2
@webdeveloper130390 <abdullokhpakhlavonov@gmail.com>
2024-06-28 07:53:24 +0500

    fixing

--- js/index.js
+++ js/index.js
@@ -1,7 +1,15 @@
-let son1 = +prompt("A sonni kiriting")
-let son2 = +prompt("B sonni kiriting")
-if(son1 >= son2){
-    alert("A son katta")
-}else{
-    alert("B son katta")
+let a = (prompt("Birinchi sonni kiriting (a):"));
+let b = (prompt("Ikkinchi sonni kiriting (b):"));
+let c = (prompt("Uchinchi sonni kiriting (c):"));
+
+let largest;
+
+if (a >= b && a >= c) {
+  largest = a;
+} else if (b >= a && b >= c) {
+  largest = b;
+} else {
+  largest = c;
 }
+
+alert("Eng katta son: " + largest);
